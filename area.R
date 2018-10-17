ae591bb5d7b50bda5da158fc4d73b83717047ac2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:06:47 -0400

    Saving area.R

--- area.R
+++ area.R
@@ -1,1 +1,9 @@
 
+US=map_data("state")
+
+options(scipen=999)
+
+Area=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="black")
+Area=Area + expand_limits(x=US$long, y=US$lat)
+Area
+
