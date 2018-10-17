e65c42034043b988046a8b8c7420203ca2d1ff8d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:13:04 -0400

    Saving murderrate.R

--- murderrate.R
+++ murderrate.R
@@ -1,4 +1,10 @@
 
-murder=ggplot(merged_data, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
+library(ggplot2)
+library(ggmap)
+
+US=map_data("state")
+
+options(scipen=999)
+murder=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 murder=murder + expand_limits(x=US$long, y=US$lat)
 murder
