5aecccd1bf8828d72f92b847f3cdf016429d72c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:00:35 -0400

    Saving census.R

--- census.R
+++ census.R
@@ -6,7 +6,7 @@
 # Portions of this code came from Introduction to Data Science
 # but the comments are all original.
 
-#read in the census data from HW #3
+#Reading in the census data 
 census <- read.csv("scprc_est_2017_18_pop_res.csv")
 census <- census[,-1:-4]
 census <- census[-c(1,53,10),]
