704d72f5b9cbfe583c328d41980b70f4f2231b19
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:04:45 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -1,18 +1,17 @@
 
 library(ggplot2)
 library(ggmap)
-$area= state.area
-View(merged_data) 
+
+merged$area= state.area
 
 statecenter= data.frame(state.center)
-View(statecenter)
-merged_data$latitude=statecenter$y
-merged_data$longitude=statecenter$x
-View(merged_data)
+
+merged$latitude=statecenter$y
+merged$longitude=statecenter$x
 
 
 
-merged_data$stateName=tolower(merged_data$stateName)
-View(merged_data)
-US=map_data("state")
 
+merged$stateName=tolower(merged_data$stateName)
+
+merged
