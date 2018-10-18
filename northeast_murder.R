b3ae270baef6b40db6a16d047cc13e080ff661de
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:59:51 -0400

    Saving northeast_murder.R

--- northeast_murder.R
+++ northeast_murder.R
@@ -7,3 +7,5 @@ US=map_data("state")
 options(scipen=999)
 northeast_murder=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 northeast_murder=northeast_murder + expand_limits(x=US$long, y=US$lat) + ggtitle("Murder rate in north east US states") + coord_map()
+northeast_murder=  murder+ xlim(-83.93, -63.93) + ylim(30.73, 50.73)
+northeast_murder
