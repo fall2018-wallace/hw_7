67b1f70d8e82c3c0788ea3c6c884240014ffcb70
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:24:18 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -2,13 +2,11 @@
 #Using the areas of US states
 merged$area= state.area
 
+#Using the longitude and latitude of US states and converting it with dataframe
 statecenter= data.frame(state.center)
 
-merged$latitude=statecenter$y
-merged$longitude=statecenter$x
-
-
-
+merged$latitude=statecenter$y #Taking a latitude as one column 
+merged$longitude=statecenter$x #Taking the longitude as other column
 
 merged$stateName=tolower(merged$stateName)
 
