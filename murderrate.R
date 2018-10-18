64208236a738a5f2721fb25f878b93099b3f05a6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:29:24 -0400

    Saving murderrate.R

--- murderrate.R
+++ murderrate.R
@@ -8,6 +8,7 @@ US=map_data("state")
 
 #Converting the scale to readable form
 options(scipen=999)
+#Plotting the map according to murder rate
 murder=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 murder=murder + expand_limits(x=US$long, y=US$lat) + ggtitle("Murder rate in US states") + coord_map()
 murder
