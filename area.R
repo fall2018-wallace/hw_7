2d3eb0130a61c3bf8a32151a8dbafae428845023
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:45:21 -0400

    Saving area.R

--- area.R
+++ area.R
@@ -6,6 +6,6 @@ US=map_data("state")
 
 options(scipen=999)
 Area=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="black")
-Area=Area + expand_limits(x=US$long, y=US$lat)
+Area=Area + expand_limits(x=US$long, y=US$lat) + ggtitle("Area of US states") + coord_map()
 Area
 
