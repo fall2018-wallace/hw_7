4c33811f10593486fa0eb8afc136f7ccb538bcf6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:55:25 -0400

    removing northeast_area.R

--- northeast_area.R
+++
@@ -1,9 +0,0 @@
-
-library(ggplot2)
-library(ggmap)
-
-US=map_data("state")
-
-options(scipen=999)
-northeast_murder=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
-northeast_murder=northeast_murder + expand_limits(x=US$long, y=US$lat) + ggtitle("Murder rate in north east US states") + coord_map()
