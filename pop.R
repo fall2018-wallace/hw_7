83d7deebb8d558e47f71113ed8477224471c4123
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 20:44:08 -0400

    Saving pop.R

--- pop.R
+++ pop.R
@@ -1,1 +1,7 @@
 
+pop=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
+pop=pop + expand_limits(x=US$long, y=US$lat)
+pop= pop + geom_point(aes(x=merged$longitude, y=merged$latitude, size=merged_data$population)) + coord_map()
+pop=pop + ggtitle("Population")
+pop
+
