2c02816e23a9551f9a280fcc33084355bc1a7f4c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:32:30 -0400

    Saving pop.R

--- pop.R
+++ pop.R
@@ -12,6 +12,8 @@ options(scipen=999)
 #Plotting the map according to the murder rate
 pop=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 pop=pop + expand_limits(x=US$long, y=US$lat)
+
+#Getting the longitude and latitude of each state as the circle at the center and size according to teh population of the state
 pop= pop+geom_point(aes(x=merged$longitude, y=merged$latitude, size=merged$population)) 
 pop=pop + ggtitle("Population") + coord_map()
 pop
