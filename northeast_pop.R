1937227f8a3b69bf0b0290b5cecd4d13d21f7d1b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:00:26 -0400

    Saving northeast_pop.R

--- northeast_pop.R
+++ northeast_pop.R
@@ -7,3 +7,5 @@ northeast_pop=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=
 northeast_pop=northeast_pop + expand_limits(x=US$long, y=US$lat)
 northeast_pop= northeast_pop+geom_point(aes(x=merged$longitude, y=merged$latitude, size=merged$population)) 
 northeast_pop=northeast_pop + ggtitle("Population in north east in US states") + coord_map()
+northeast_pop= northeast_pop + xlim(-83.93, -63.93) + ylim(30.73, 50.73)
+northeast_pop
