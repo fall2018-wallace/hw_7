2a5e7c618ae7392f678d2c208cbbaa0aaf30119f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:27:03 -0400

    Saving area.R

--- area.R
+++ area.R
@@ -9,6 +9,7 @@ US=map_data("state")
 #Converting the scale to readable form
 options(scipen=999)
 
+#plotting the mapaccording to area
 Area=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=area),color="black")
 Area=Area + expand_limits(x=US$long, y=US$lat) + ggtitle("Area of US states") + coord_map()
 Area
