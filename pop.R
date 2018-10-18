7aba4aa61a300c37c3329c2b43b76f038fdb19de
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:30:50 -0400

    Saving pop.R

--- pop.R
+++ pop.R
@@ -2,7 +2,10 @@
 #Importing the required libraries
 library(ggplot2)
 library(ggmap)
+
+#geting the US state data for plotting the map
 US=map_data("state")
+
 options(scipen=999)
 pop=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 pop=pop + expand_limits(x=US$long, y=US$lat)
