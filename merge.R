f8926db4bfa3f25104006478dc59ea42ff570a4b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:22:03 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,5 +1,5 @@
 
 #Merge the two dataframes
-merged=merge(census, arrests)
+merged=merge(census,arrests) #Merging census and arrests dataframes
 colnames(merged)=c("stateName","population","popOver18","percentOver18","Murder","Assault","UrbanPop","Rape")
 merged
