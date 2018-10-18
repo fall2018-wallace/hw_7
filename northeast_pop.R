d0b076161dd73a8ddcf91ca441db78e844192760
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:39:11 -0400

    Saving northeast_pop.R

--- northeast_pop.R
+++ northeast_pop.R
@@ -12,6 +12,8 @@ options(scipen=999)
 #plotting the murder rate using map
 northeast_pop=ggplot(merged, aes(map_id=stateName)) + geom_map(map=US, aes(fill=Murder),color="black")
 northeast_pop=northeast_pop + expand_limits(x=US$long, y=US$lat)
+
+#Plotting the population of each state as per the size of the circle
 northeast_pop= northeast_pop+geom_point(aes(x=merged$longitude, y=merged$latitude, size=merged$population)) 
 northeast_pop=northeast_pop + ggtitle("Population in north east in US states") + coord_map()
 northeast_pop= northeast_pop + xlim(-83.93, -63.93) + ylim(30.73, 50.73)
